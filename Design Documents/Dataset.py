32796e7863324385910069897616cd2c877398be
Sai Amartya Balamurugan Lakshmipraba <saiamartya19@gmail.com>
2024-12-09 09:11:45 -0500

    Update Dataset.py

--- Design Documents/Dataset.py
+++ Design Documents/Dataset.py
@@ -1,15 +1,1 @@
-# Data flow:
-# L-channel input -> Encoder (extract features) -> Decoder (predict A/B) -> Output predicted A/B channels
 
-# class ColorizationNetwork(nn.Module):
-    # __init__:
-
-    #     - Define layers: convolutional blocks for encoder, upsampling layers for decoder.
-    #
-    
-    # forward(x):
-    #     - Pass input L-channel through encoder layers.
-
-    #     - Decode features to produce A/B output channels.
-
-    #     - Return predicted A/B tensor.
