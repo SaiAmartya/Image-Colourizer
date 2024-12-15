1f183cbb4436a34ed24ac24eba9e3d70f8518e93
Sai Amartya Balamurugan Lakshmipraba <saiamartya19@gmail.com>
2024-12-14 23:31:36 -0500

    Update testing.py

---
+++ Design Documents/testing.py
@@ -0,0 +1,22 @@
+# Device configuration
+
+# Load test data
+
+# Load the trained model
+
+# Visualize results function
+  # Convert tensors -> numpy arrays
+
+  # Create ground truth and predicted color images
+
+  # Plot grayscale, true rgb and predicted lab images
+
+# Evaluate the model through a loss function
+
+  # Forward pass
+
+  # Loss computed per batch
+
+  # Visualize results
+
+# Display average loss
