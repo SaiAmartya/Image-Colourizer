1083af642c2b73f30b745b688026aedf72798bd8
Sai Amartya Balamurugan Lakshmipraba <saiamartya19@gmail.com>
2024-12-14 23:21:00 -0500

    Update training.py

---
+++ Design Documents/training.py
@@ -0,0 +1,19 @@
+# Hyperparemeters
+
+# Device configuration (what to use for training)
+
+# Load data
+
+# initialize model, loss function & optimizer
+
+# training loop
+  # Grayscale input
+  # Colour target
+
+  # Forward Pass
+
+  # Backward Pass
+
+# Print epoch loss
+
+# Save trained model
