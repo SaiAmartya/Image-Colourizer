ed8aacc7af23ba073c2d7805b110dcefed1d960f
Sai Amartya Balamurugan Lakshmipraba <saiamartya19@gmail.com>
2024-12-14 23:35:53 -0500

    Update main.py

---
+++ Design Documents/main.py
@@ -0,0 +1,11 @@
+# import argparse
+# from training import train
+# from testing import test
+
+# Main function
+  # set up argument parser
+  # define argument to either training or testing
+
+  # check the model and call the appropriate function
+
+# Ensure the script runs only when its directly executed
